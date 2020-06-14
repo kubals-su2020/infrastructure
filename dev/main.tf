b88e570de152b1aaccc24dd6c2b0b45c2f176c24
kubal-s <kubal.s@husky.neu.edu>
2020-06-13 20:05:52 -0400

    added some steps for terraform

--- dev/main.tf
+++ dev/main.tf
@@ -4,6 +4,11 @@ module "vpc_1" {
     tenancy = "default"
     subnet_cidr = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
     tag = "a4"
+
+    # create_database_subnet_group = true
+    # database_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"] 
+
+
 }
 
 # module "vpc_2" {
